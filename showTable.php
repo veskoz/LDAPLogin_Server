8cfcfffe7772bde457614ce784bd6d98558267f1
Andrea Vescovi <vescosauro@hotmail.it>
2020-07-27 20:16:03 +0200

    moved to table samples instead of names

--- showTable.php
+++ showTable.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM names";
+$sql = "SELECT * FROM samples";
 $result = $conn->query($sql);
 
 echo "SQL: " . $sql . "<br>";
