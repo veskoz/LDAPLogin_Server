7b14c4b3284cc2d1180e41f8ffcc78056a591c51
Andrea Vescovi <vescosauro@hotmail.it>
2020-07-27 16:48:08 +0200

    fixed indentation

--- showTable.php
+++ showTable.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
@@ -12,7 +13,7 @@ $dbname = "android";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT * FROM names";
@@ -21,12 +22,12 @@ $result = $conn->query($sql);
 echo "SQL: " . $sql . "<br>";
 //date use seconds so $row["time"] has to be divided by 1000
 if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"] . " barcode: " . $row["barcode"] . " time: " .  date("F j, Y, g:i a",$row["time"]/1000) . " ldap_user: " .  $row["ldap_user"] . "<br>";
-  }
+    // output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"] . " barcode: " . $row["barcode"] . " time: " . date("F j, Y, g:i a", $row["time"] / 1000) . " ldap_user: " . $row["ldap_user"] . "<br>";
+    }
 } else {
-  echo "0 results";
+    echo "0 results";
 }
 $conn->close();
 ?> 
